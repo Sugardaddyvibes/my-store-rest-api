721e02d5039090a40d459f7727a95b41ad886c2e
Akinwale Adeyeri <94736154+Sugardaddyvibes@users.noreply.github.com>
2022-08-10 00:02:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from resources.items import Item,ItemList
 from resources.store import Store,StoreList
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
 app.secret_key='jose'  
@@ -23,12 +24,14 @@ api.add_resource(StoreList,'/stores')
 if __name__=='__main__':
     from db import db
     db.init_app(app)
-  
-    @app.before_first_request 
-    def create_tables():
-        db.create_all() 
+    
+    
+    if app.config['DEBUG']: 
+        @app.before_first_request 
+        def create_tables():
+            db.create_all() 
  
  
-    app.run(port=5000,, debug=True) 
+    app.run(port=5000) 
 
 
